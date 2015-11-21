d9403f3933611b80aa7699c3b51f29cca3496879
Hawaiii <yihua25@gmail.com>
2015-11-21 07:38:20 -0500

    valid csv file

--- svm/baseline.m
+++ svm/baseline.m
@@ -3,28 +3,44 @@
 % eventsTrain: divde into 1:351, and 352:702 two scanning runs
 % x,y,z: 3D coordinate of fMRI scans
 
-% TODO 20% - valid csv
+% 20% - valid csv
 % TODO (20+30)% - baseline svm
 % TODO 100% - 66.7 accurancy
 % TODO extracredit
 
 load('../data/Train.mat');
-% load('../data/Test.mat');
-nClass = size(unique(YTrain),1);
-nTest = size(XTest,1);
+load('../data/Test.mat');
+nClass = size(unique(Ytrain),1);
+nTest = size(Xtest,1);
 
-% TODO Train baseline
+%% TODO Train baseline
+% TODO Set the hinge weight C=50 in the objective wTw/2+C?ih(yixi?w).
+select01 = (Ytrain==0)|(Ytrain==1);
+svm01 = svmtrain(Xtrain(select01,:),Ytrain(select01), ...
+    'kernel_function', 'rbf','rbf_sigma',100);
+select03 = (Ytrain==0)|(Ytrain==3);
+svm03 = svmtrain(Xtrain(select03,:),Ytrain(select03), ...
+    'kernel_function', 'rbf','rbf_sigma',100);
+select13 = (Ytrain==1)|(Ytrain==3);
+svm13 = svmtrain(Xtrain(select13,:),Ytrain(select13), ...
+    'kernel_function', 'rbf','rbf_sigma',100);
 
-% From the inputs, use only X (the voxel array).
-% Use a Gaussian RBF kernel, i.e., k(x,x?)=exp(??x?x??/2?2).
-% Set kernel width ?=100.
-% Set the hinge weight C=50 in the objective wTw/2+C?ih(yixi?w).
-% Fit three binary 1-vs-1 classifiers (one for each pair of classes).
+%% Test
 % When testing, use majority vote among the three classifiers: e.g., if 
 % the 0-1 classifier says 1, the 0-3 classifier says 3, and the 1-3 
 % classifier says 1, predict 1.
+pred01 = svmclassify(svm01, Xtest);
+pred03 = svmclassify(svm03, Xtest);
+pred13 = svmclassify(svm13, Xtest);
 
-% TODO Test
-pred = zeros(nTest, nClass);
+pred = ones(nTest, nClass)/3;
+pred((pred01==0) & (pred03 ==0), 1) = 1;
+pred((pred01==0) & (pred03 ==0), 2:3) = 0;
+pred((pred01==1) & (pred13 ==1), 1) = 0;
+pred((pred01==1) & (pred13 ==1), 2) = 1;
+pred((pred01==1) & (pred13 ==1), 3) = 0;
+pred((pred03==3) & (pred13 ==3), 1:2) = 0;
+pred((pred03==3) & (pred13 ==3), 3) = 1;
 
-% TODO Write prediction.csv
+% Write prediction.csv
+csvwrite('prediction.csv',pred);
