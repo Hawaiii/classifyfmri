d804115de9ceb120b9614f883091b4a435b2443f
Karandeep Johar <karandeepjohar@gmail.com>
2015-11-24 00:08:22 -0500

    knn

--- knn/knn.m
+++ knn/knn.m
@@ -17,32 +17,59 @@ nSubject = max(subjectsTrain(:));
 %% Train
 
 % Trying PCA
-CUTOFF = 50;
-[coeff,score,latent] = pca(Xtrain);
-trainavg = mean(Xtrain,1);
-Xtrain = score(:,1:CUTOFF);
-SubClassifier = cell(1, nSubject); %An cell array
-for subID = 1:nSubject
-    idx = (subjectsTrain==subID);
-    if sum(idx) > 0
-       	SubClassifier{1,subID} = fitcknn(Xtrain(idx, :),Ytrain(idx, 1),'NumNeighbors',3);
-    end
-end
+
+% CUTOFF = 200;
+% [coeff,score,latent] = pca(Xtrain);
+% Xtrain = score(:,1:CUTOFF);
+% prefix = 'subjectID'
+% subjects = unique(subjectsTrain)
+% for i = 1:size(subjects,1)
+% 	subID = subjects(i);
+% 	% subID
+% 	% strcat(prefix,int2str(subID))
+% 	assignin('base',strcat(prefix,int2str(subID)),fitcknn(Xtrain(subjectsTrain(:, 1) == subID, :),Ytrain(subjectsTrain(:, 1) == subID, 1),'NumNeighbors',3));
+% end
 
 % SubClassifier
-% mdl = fitcknn(Xtrain,Ytrain,'NumNeighbors',1);
-% mdl
-% rloss = resubLoss(mdl)
-% cvmdl = crossval(mdl);
-% kloss = kfoldLoss(cvmdl)
-Xtest = (Xtest-repmat(trainavg,size(Xtest,1),1)) * coeff(:,1:CUTOFF);
-pred013 = zeros(nTest,1);
-for i=1:nTest
-	classifier = SubClassifier{subjectsTest(i,1)};
-	pred013(i,1) = predict(classifier,Xtest(i,:));
+minkloss = 100000000;
+
+for i=1:10
+	mdl = fitcknn(Xtrain,Ytrain,'NumNeighbors',i,'NSMethod','exhaustive','Distance','correlation');
+	% mdl
+	% rloss = resubLoss(mdl)
+	cvmdl = crossval(mdl);
+	kloss = kfoldLoss(cvmdl)
+	if kloss<minkloss
+		minmdl = mdl;
+		minkloss = kloss;
+	end
 end
+mdl = minmdl
+% Xtest = Xtest * coeff(:,1:CUTOFF);
+% for i=1:nTest
+	
+% 	testID = num2str(subjectsTest(i,1));
+% 	classifier = eval(strcat(prefix, testID));
+% 	% throwing error
+% 	% 	Error using predict (line 84)
+% 	% Systems of cell class cannot be used with the "predict" command. Convert the system to an identified model first, such as by using the "idss"
+% 	% command.
+
+% 	% Error in knn (line 39)
+% 	% 	pred013(i) = predict(classifier,Xtest(i,:))
+% 	pred013(i) = predict(classifier,Xtest(i,:));
+% end
+pred013 = predict(mdl,Xtest);
+
 pred = zeros(nTest, nClass);
 pred((pred013==0),1) = 1;
 pred((pred013==1),2) = 1;
 pred((pred013==3),3) = 1;
-csvwrite('prediction.csv',pred);
+
+pred((pred013==3),1:2) = 0;
+pred;
+pred_knn =pred;
+% Write prediction.csv
+save('pred.mat','pred_knn');
+csvwrite('prediction.csv',pred)
+
