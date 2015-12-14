d99c5dab0c7d4d8c5f9f537b4b38816e90edddab
Hawaiii <yihua25@gmail.com>
2015-12-14 16:25:55 -0500

    tuned parameters for pca and knn approach: not great results

--- part2/baseline/usePCA.m
+++ part2/baseline/usePCA.m
@@ -2,7 +2,7 @@
 
 load('../../data/provideIdx.mat');
 load('../../data/missIdx.mat');
-load('../../data/provideData_1000.mat');
+% load('../../data/provideData_1000.mat');
 % load('../../data/events_1000.mat');
 load('../../data/Train.mat');
 load('../../data/Test.mat');
@@ -10,31 +10,56 @@ NVOXEL = 5903; % total number of voxels
 NMISSING = size(missIdx,2);
 NDATA = size(provideData,1);
 
-pred = zeros(1000, 2731); % result prediction to write
 
 %% Do prediction here: using PCA
-CUTOFF = 100;
-K = 3;
+rmse = zeros(16,3); rmseptr = 1;
+testRatio = 0.1;
+for CUTOFF = 10: 50:200
+    for K = 1:5:20
+        % project train data onto pca
+        fullData = vertcat(Xtrain, Xtest);
 
-% project train data onto pca
-completeData = vertcat(Xtrain, Xtest);
-completeDataS = completeData(:,provideIdx);
-xmean = mean(completeDataS,1);
-[coeff,score,latent] = pca(completeDataS);
-completeDataSPCA = score(:,1:CUTOFF);
+        trainidx = randperm(size(fullData, 1));
+        trainCutoff = round((1-testRatio)*size(fullData,1));
+        trainFullData = fullData(trainidx(1:trainCutoff),:);
+        testFullData = fullData(trainidx(trainCutoff+1:end),:);
 
-% project test data onto pca
-missingDataPCA = (provideData-repmat(xmean, size(provideData,1), 1)) * coeff(:,1:CUTOFF);
+        pred = zeros(size(testFullData,1), 2731); % result prediction to write
+        
+        fullDataS = trainFullData(:,provideIdx);
+        xmean = mean(fullDataS,1);
+        [coeff,score,latent] = pca(fullDataS);
+        fullDataSPCA = score(:,1:CUTOFF);
 
-% calculate distance and do prediction
-dist = pdist2(missingDataPCA,completeDataSPCA);
-[dist, idx] = sort(dist, 2, 'ascend');
-for i = 1:size(pred,1)
-    for j = 1:K
-        pred(i,:) = pred(i,:)+completeData(idx(i,j), missIdx);
+        % project test data onto pca
+        provideData = testFullData(:,provideIdx);
+        missingDataPCA = (provideData-repmat(xmean, size(provideData,1), 1)) * coeff(:,1:CUTOFF);
+
+        % calculate distance and do prediction
+        dist = pdist2(missingDataPCA,fullDataSPCA);
+        [dist, idx] = sort(dist, 2, 'ascend');
+        for i = 1:size(pred,1)
+            for j = 1:K
+                pred(i,:) = pred(i,:)+fullData(idx(i,j), missIdx);
+            end
+        end
+        pred = pred/K;
+
+        % calculate RMSE
+        rmse(rmseptr,1) = CUTOFF;
+        rmse(rmseptr,2) = K;
+        rmse(rmseptr,3) = sqrt( sum(sum( (testFullData(:,missIdx)-pred).^2)) / numel(pred) );
+        
+        rmseptr = rmseptr+1;
     end
 end
-pred = pred/K;
+
+figure;
+scatter3(rmse(:,1), rmse(:,2), rmse(:,3))
+xlabel('cutoff')
+ylabel('k')
+title('rmse')
+
 %%
-csvwrite('prediction.csv',pred); % remember to compress to zip before turning into Autolab!
+% csvwrite('prediction.csv',pred); % remember to compress to zip before turning into Autolab!
 % zip prediction.csv.zip prediction.csv
