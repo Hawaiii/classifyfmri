f10d2d68bea5bf8534ee9d8f56a1240270bd1311
Hawaiii <yihua25@gmail.com>
2015-12-18 13:54:03 -0500

    change for each class

--- part3/brainActivityIntensity.m
+++ part3/brainActivityIntensity.m
@@ -1,14 +1,22 @@
 % for each subject
 % calculate the brain activity for each time
 
-subjectID = 6; %1-4,6-28
+% subjectID = 6; %1-4,6-28
+label = 3; % 0, 1, 3
 load('../data/Train.mat');
 load('../data/Test.mat');
+load('../data/YTest.mat');
 
-trainSelect = subjectsTrain == subjectID;
-testSelect = subjectsTest == subjectID;
+% trainSelect = subjectsTrain == subjectID;
+% testSelect = subjectsTest == subjectID;
+% X = vertcat(Xtrain(trainSelect,:), Xtest(testSelect,:));
+% time = vertcat(eventsTrain(trainSelect,:), eventsTest(testSelect,:));
+
+trainSelect = Ytrain == label;
+testSelect = Ytest == label;
 X = vertcat(Xtrain(trainSelect,:), Xtest(testSelect,:));
 time = vertcat(eventsTrain(trainSelect,:), eventsTest(testSelect,:));
+
 
 [time, idx] = sort(time);
 ordX = X(idx,:);
