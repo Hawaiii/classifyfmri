49a8852635a739825b87aad1cfa098bba432eff1
Hawaiii <yihua25@gmail.com>
2015-12-18 17:01:19 -0500

    new plots

--- part3/brainActivityIntensity.m
+++ part3/brainActivityIntensity.m
@@ -2,15 +2,10 @@
 % calculate the brain activity for each time
 
 % subjectID = 6; %1-4,6-28
-label = 3; % 0, 1, 3
-% load('../data/Train.mat');
-% load('../data/Test.mat');
-% load('../data/YTest.mat');
-
-% trainSelect = subjectsTrain == subjectID;
-% testSelect = subjectsTest == subjectID;
-% X = vertcat(Xtrain(trainSelect,:), Xtest(testSelect,:));
-% time = vertcat(eventsTrain(trainSelect,:), eventsTest(testSelect,:));
+label = 1; % 0, 1, 3
+load('../data/Train.mat');
+load('../data/Test.mat');
+load('../data/YTest.mat');
 
 trainSelect = Ytrain == label;
 testSelect = Ytest == label;
@@ -24,11 +19,20 @@ numscan = size(time,1);
 
 change = zeros(max(time),size(ordX,2));
 for i = 2:numscan
-    tmp = (ordX(i,:)-ordX(i-1,:))/(time(i)-time(i-1));
+    tmp = abs((ordX(i,:)-ordX(i-1,:))/(time(i)-time(i-1)));
     for j = time(i-1):time(i)
         change(j,:) = tmp;
     end
 end
 
+% smoothing it
+gaussFilter = gausswin(31);
+gaussFilter = gaussFilter / sum(gaussFilter); % Normalize.
+
+% Do the blur.
 changesum = sum(change,2);
-plot(changesum);
+ssum = conv(changesum, gaussFilter)
+
+plot(ssum);
+xlabel('time')
+ylabel('change')
